25f2154ef3a8bf52f19d7dd5924867812331ca99
Manuel Muñoz <manuel.munoz@foonkiemonkey.co.uk>
2020-11-19 00:38:59 -0500

    pantalla de perfil

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'auth/loginPage.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _screenSizeWidth = MediaQuery.of(context).size.width;
     final _fontScaling = MediaQuery.of(context).textScaleFactor;
+
+    Widget _titleText() => Container(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Tu perfil',
+            style: TextStyle(fontSize: _fontScaling / 0.038),
+          ),
+        );
+
+    Widget _userInformation() => Center(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            SvgPicture.asset('assets/user.svg',
+                fit: BoxFit.cover,
+                color: Theme.of(context).accentColor,
+                height: _screenSizeWidth / 4.5),
+            SizedBox(height: _screenSizeWidth / 20),
+            Text('Nombre Apellido'),
+            SizedBox(height: _screenSizeWidth / 40),
+            Text('Direccion'),
+            SizedBox(height: _screenSizeWidth / 40),
+            Text('Ciudad')
+          ]),
+        );
+
+    Widget _userPublishedArticles() => GestureDetector(
+          child: Row(
+            children: [
+              Icon(
+                Icons.add_shopping_cart,
+                color: Theme.of(context).accentColor,
+              ),
+              SizedBox(width: _screenSizeWidth / 20),
+              Text('Artículos publicados')
+            ],
+          ),
+          onTap: () {
+            print('hola');
+          },
+        );
+
+    Widget _acquireArticles() => GestureDetector(
+          child: Row(
+            children: [
+              Icon(
+                Icons.accessible_forward,
+                color: Theme.of(context).accentColor,
+              ),
+              SizedBox(width: _screenSizeWidth / 20),
+              Text('Articulos adquiridos')
+            ],
+          ),
+          onTap: () {
+            print('hola');
+          },
+        );
+
+    Widget _logout() => GestureDetector(
+          child: Row(children: [
+            Icon(
+              Icons.logout,
+              color: Theme.of(context).accentColor,
+            ),
+            SizedBox(width: _screenSizeWidth / 20),
+            Text('Cerrar sesión')
+          ]),
+          onTap: () {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (_) => LoginPage()),
+                (Route<dynamic> route) => false);
+          },
+        );
 
     /**
      * 
@@ -16,46 +91,28 @@ class ProfilePage extends StatelessWidget {
         child: Scaffold(
             body: Padding(
           padding: EdgeInsets.symmetric(horizontal: _screenSizeWidth / 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(children: [
-                ClipOval(
-                  child: Icon(Icons.person_pin,
-                      size: _screenSizeWidth / 5,
-                      color: Theme.of(context).accentColor),
-                  // child: Image.network('https://via.placeholder.com/150',
-                  //     height: _screenSizeWidth / 5,
-                  //     width: _screenSizeWidth / 5,
-                  //     fit: BoxFit.cover),
-                ),
-                SizedBox(width: _screenSizeWidth / 20),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Text('Nombre Apellido'),
-                  SizedBox(height: _screenSizeWidth / 40),
-                  Text('Direccion'),
-                  SizedBox(height: _screenSizeWidth / 40),
-                  Text('Ciudad')
-                ]),
-              ]),
-              SizedBox(height: _screenSizeWidth / 8),
-              GestureDetector(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.article,
-                      color: Theme.of(context).accentColor,
-                    ),
-                    SizedBox(width: _screenSizeWidth / 20),
-                    Text('Artículos publicados')
-                  ],
-                ),
-                onTap: () {
-                  print('hola');
-                },
-              ),
-            ],
-          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            _titleText(),
+            SizedBox(height: _screenSizeWidth / 10),
+            _userInformation(),
+            SizedBox(height: _screenSizeWidth / 10),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Container(
+                    height: _screenSizeWidth / 700.0, color: Colors.grey[400])),
+            SizedBox(height: _screenSizeWidth / 10),
+            _userPublishedArticles(),
+            SizedBox(height: _screenSizeWidth / 20),
+            _acquireArticles(),
+            SizedBox(height: _screenSizeWidth / 3),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Container(
+                    height: _screenSizeWidth / 700.0, color: Colors.grey[400])),
+            SizedBox(height: _screenSizeWidth / 20),
+            _logout()
+          ]),
         )),
       ),
     );
