4aa2b93c7337923a4c0a572178d15c054a9d082b
Manuel Muñoz <manuel.munoz@foonkiemonkey.co.uk>
2021-08-05 12:03:55 -0500

    Add about document

--- lib/pages/about/about.dart
+++ lib/pages/about/about.dart
@@ -11,7 +11,7 @@ class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return PDFWidget(
-      path: 'assets/docs/termsConditions.pdf',
+      path: 'assets/docs/about.pdf',
     );
   }
 }
